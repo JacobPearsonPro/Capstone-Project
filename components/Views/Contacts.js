676a452087ad73725b627c74e5f90073d19e00bb
Jacob Pearson <JacobPearsonPro@gmail.com>
2021-11-08 18:17:41 -0600

    Views update for Demo Day

--- components/Views/Contacts.js
+++ components/Views/Contacts.js
@@ -2,6 +2,18 @@ import html from "html-literal";
 
 export default st => html`
   <main id="Contacts">
-    <p>https://github.com/JacobPearsonPro/</p>
+    <h1>Ways to reach me</h1>
+    <p>You may find me at any of the following.</p>
+    <ul>
+      <li><a href="https://github.com/JacobPearsonPro">Github</a></li>
+      <li>
+        <a href="https://www.linkedin.com/in/jacob-pearson-a87a45149/"
+          >Linkdin</a
+        >
+      </li>
+      <li>
+        <a href="https://JacobPearsonPro@gmail.com">Gmail</a>
+      </li>
+    </ul>
   </main>
 `;
